9516c291e41febed4232d2f75b648a56f8b361b0
Eric <834437543@qq.com>
2019-02-12 21:43:46 -0500

    zz

---
+++ txt2jsonConverter.py
@@ -0,0 +1,30 @@
+import json
+import numpy as np
+
+nodeList = []
+linkStrList = ''
+with open('BerkStanf_4096_8192.txt','r') as f:
+    tmp = f.read()
+    tmp_ = tmp.replace('\n',' ')
+    tmp__ = tmp_.split(' ')
+    tmp__ = list(map(int, tmp__))
+    nodeList = np.unique(tmp__)
+def genNodes():
+    nodeStrList = ''
+    for i in nodeList:
+        nodeStr = '{\"id\":\"' + str(i) + '\", \"group\":1}'
+        nodeStrList = nodeStrList + ',' + nodeStr
+    return nodeStrList
+nodeStrList = genNodes().lstrip(',')
+with open('BerkStanf_4096_8192.txt','r') as f:
+    tmp = f.readlines()
+    for i in tmp:
+        i = i.replace('\n','')
+        i = i.split(' ')
+        linkStrList = linkStrList + ',' + '{\"source\": \"' + i[0] + '\", \"target\": \"' + i[1] + '\", \"value\": 1}'
+linkStrList = linkStrList.lstrip(',')
+jsonStrFile = ''
+jsonStrFile = '{\"nodes\":[' + nodeStrList + '],\"links\":[' + linkStrList + ']}'
+with open('graphFile.json','w') as f:
+    f.write(jsonStrFile)
+print(tmp__)
